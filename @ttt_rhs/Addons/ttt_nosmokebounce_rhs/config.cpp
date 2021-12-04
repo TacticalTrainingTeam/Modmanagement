ea464808ef5727c76f05b24f476feb135edbb0c7
V1irus <antivirus176@gmail.com>
2021-12-04 23:32:40 +0100

    nosmokebounce fix

--- @ttt_rhs/Addons/ttt_nosmokebounce_rhs/config.cpp
+++ @ttt_rhs/Addons/ttt_nosmokebounce_rhs/config.cpp
@@ -12,72 +12,49 @@ class CfgPatches
 		version=1;
 		versionStr=1;
 		versionAr[]={1,0};
-		author="[A] Willard, [DAA] dedmen, [TTT]";
-		ammo[]=
-		{
-			"rhs_40mm_smoke_white",
-			"rhs_40mm_m714_white",
-			"rhs_40mm_smoke_red",
-			"rhs_40mm_m713_red",
-			"rhs_40mm_smoke_green",
-			"rhs_40mm_m715_green",
-			"rhs_40mm_smoke_yellow",
-			"rhs_40mm_m716_yellow",
-			"rhs_g_vg40md_white"
-		};
+		author="[TTT]";
 	};
 };
-class CfgAmmo
-{
-	class SmokeShell;
-	class rhs_40mm_smoke_white: SmokeShell
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
-	};
-	class rhs_40mm_m714_white: SmokeShell
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
+class CfgAmmo {
+
+
+	// RHS american smoke grenades
+	class SmokeShellGreen;
+	class rhs_40mm_m715_Green: SmokeShellGreen {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
 	};
 	class SmokeShellRed;
-	class rhs_40mm_smoke_red: SmokeShellRed
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
+	class rhs_40mm_m713_Red: SmokeShellRed {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
 	};
-	class rhs_40mm_m713_red: SmokeShellRed
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
-	};
-	class SmokeShellGreen;
-	class rhs_40mm_smoke_green: SmokeShellGreen
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
-	};
-	class rhs_40mm_m715_green: SmokeShellGreen
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
+	class SmokeShell;
+	class rhs_40mm_m714_White: SmokeShell {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
 	};
 	class SmokeShellYellow;
-	class rhs_40mm_smoke_yellow: SmokeShellYellow
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
+	class rhs_40mm_m716_yellow: SmokeShellYellow {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
 	};
-	class rhs_40mm_m716_yellow: SmokeShellYellow
-	{
-		simulation="shotSmoke";
-		deflectionSlowDown=0.40000001;
-	};
+
+	// RHS russian smoke grenades
 	class rhs_g_vog25;
-	class rhs_g_vg40md_white: rhs_g_vog25
-	{
-		simulation="shotSmoke";
-		deflecting=30;
-		deflectionSlowDown=0.40000001;
+
+	class rhs_g_vg40md_green: rhs_g_vog25 {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
+	};
+
+	class rhs_g_vg40md_red: rhs_g_vog25 {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
+	};
+
+	class rhs_g_vg40md_white: rhs_g_vog25 {
+		simulation = "shotSmoke";
+		deflectionSlowDown = 0;
 	};
 };
