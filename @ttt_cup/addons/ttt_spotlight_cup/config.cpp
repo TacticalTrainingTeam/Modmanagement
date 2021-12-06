335291a62d488062cb08fcf8748f4b7f3c28d24f
V1irus <antivirus176@gmail.com>
2021-12-06 19:20:32 +0100

    fix for spotlight RHS/CUP

--- @ttt_cup/addons/ttt_spotlight_cup/config.cpp
+++ @ttt_cup/addons/ttt_spotlight_cup/config.cpp
@@ -21,9 +21,9 @@ class RscDisplayMain: RscStandardDisplay
 		{
 			text = "Tactical Training Team - CUP Server"; // Text displayed on the square button, converted to upper-case
 			textIsQuote = 0; // 1 to add quotation marks around the text
-			picture = "ttt_spotlight_cup\button.paa"; // Square picture, ideally 512x512
+			picture = "ttt_cup\ttt_spotlight_cup\button.paa"; // Square picture, ideally 512x512
 			//video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
-			action = "0 = [_this, 'game.tacticalteam.de', '2322', '130'] execVM '\ttt_spotlight_cup\joinServer.sqf';";
+			action = "0 = [_this, 'game.tacticalteam.de', '2322', '130'] execVM 'ttt_cup\ttt_spotlight_cup\joinServer.sqf';";
 			actionText = "Join server: Tactical Training Team - CUP Server"; // Text displayed in top left corner of on-hover white frame
 			condition = "true"; // Condition for showing the spotlight
 		};
