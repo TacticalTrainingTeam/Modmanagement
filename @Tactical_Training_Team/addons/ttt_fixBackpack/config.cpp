9360e874cde310099afb9381edc87989e3bc3aa0
Andx <andx667@protonmail.com>
2024-10-22 08:33:38 +0200

    removed unneeded debug items

--- @Tactical_Training_Team/addons/ttt_fixBackpack/config.cpp
+++ @Tactical_Training_Team/addons/ttt_fixBackpack/config.cpp
@@ -59,41 +59,5 @@ class CfgVehicles
 		maximumLoad = 320;
 		mass = 50;
 	};
-	class B_CivilianBackpack_01_Base_F: Bag_Base
-	{
-	};	
-	class B_CivilianBackpack_01_Everyday_Black_F: B_CivilianBackpack_01_Base_F
-	{
-	};	
-	class debug_pack: B_CivilianBackpack_01_Everyday_Black_F
-	{
-		maximumLoad = 1337;
-		mass = 1;
-	};
-
-	class B_TacticalPack_Base: Bag_Base
-	{
-	};
-	class B_TacticalPack_blk: B_TacticalPack_Base
-	{
-	};	
-	class debug_pack2: B_TacticalPack_blk
-	{
-		maximumLoad = 9001;
-		mass = 1;
-	};
-
-	class B_ViperHarness_base_F: Bag_Base
-	{
-	};
-	class B_ViperHarness_blk_F: B_ViperHarness_base_F
-	{
-	};	
-	class debug_pack3: B_ViperHarness_blk_F
-	{
-		maximumLoad = 9001;
-		mass = 1;
-	};
-
 
 };
